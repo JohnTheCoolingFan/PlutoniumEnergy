2770717b5393eefac04498a1aee11cdbacc2c06a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-03-07 15:27:53 +0300

    Changed setting name and description

--- settings.lua
+++ settings.lua
@@ -1,10 +1,10 @@
 data:extend({
 	{
 		type = "bool-setting",
-		name = "PE-old-MOX-fuel-icon",
+		name = "PE-old-icons",
 		setting_type = "startup",
-		localised_name = {"mod-setting-name.PE-old-MOX-fuel-icon"},
-		localised_description = {"mod-setting-description.PE-old-MOX-fuel-icon"},
+		localised_name = {"mod-setting-name.PE-old-icons"},
+		localised_description = {"mod-setting-description.PE-old-icons"},
 		default_value = false
 	}
 })
