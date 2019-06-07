485f3372f343a05bd3c70aa8ed70564cff90ae10
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-06-08 01:12:24 +0300

    Code formatting

--- prototypes/entity/projectiles.lua
+++ prototypes/entity/projectiles.lua
@@ -42,137 +42,120 @@ data:extend(projectiles)
 
 data:extend({
 	{
-    type = "projectile",
-    name = "plutonium-cannon-projectile",
-    flags = {"not-on-map"},
-    collision_box = {{-0.3, -1.1}, {0.3, 1.1}},
-    acceleration = 0,
-    direction_only = true,
-    piercing_damage = 600,
-    action =
-    {
-      type = "direct",
-      action_delivery =
-      {
-        type = "instant",
-        target_effects =
-        {
-          {
-            type = "damage",
-            damage = {amount = 600 , type = "physical"}
-          },
-          {
-            type = "damage",
-            damage = {amount = 300 , type = "explosion"}
-          },
-          {
-            type = "create-entity",
-            entity_name = "plutonium-cannon-explosion"
-          }
-        }
-      }
-    },
-    final_action =
-    {
-      type = "direct",
-      action_delivery =
-      {
-        type = "instant",
-        target_effects =
-        {
-          {
-            type = "create-entity",
-            entity_name = "small-scorchmark",
-            check_buildability = true
-          }
-        }
-      }
-    },
-    animation =
-    {
-      filename = "__base__/graphics/entity/bullet/bullet.png",
-      frame_count = 1,
-      width = 3,
-      height = 50,
-      priority = "high"
-    }
-  },
-  {
-    type = "projectile",
-    name = "explosive-plutonium-cannon-projectile",
-    flags = {"not-on-map"},
-    collision_box = {{-0.3, -1.1}, {0.3, 1.1}},
-    acceleration = 0,
-    piercing_damage = 150,
-    action =
-    {
-      type = "direct",
-      action_delivery =
-      {
-        type = "instant",
-        target_effects =
-        {
-          {
-            type = "damage",
-            damage = {amount = 525 , type = "physical"}
-          },
-          {
-            type = "create-entity",
-            entity_name = "plutonium-cannon-explosion"
-          }
-        }
-      }
-    },
-    final_action =
-    {
-      type = "direct",
-      action_delivery =
-      {
-        type = "instant",
-        target_effects =
-        {
-          {
-            type = "create-entity",
-            entity_name = "plutonium-cannon-shell-explosion"
-          },
-          {
-            type = "nested-result",
-            action =
-            {
-              type = "area",
-              radius = 4.25,
-              action_delivery =
-              {
+        type = "projectile",
+        name = "plutonium-cannon-projectile",
+        flags = {"not-on-map"},
+        collision_box = {{-0.3, -1.1}, {0.3, 1.1}},
+        acceleration = 0,
+        direction_only = true,
+        piercing_damage = 600,
+        action = {
+            type = "direct",
+            action_delivery = {
                 type = "instant",
-                target_effects =
-                {
-                  {
-                    type = "damage",
-                    damage = {amount = 475, type = "explosion"}
-                  },
-                  {
-                    type = "create-entity",
-                    entity_name = "plutonium-cannon-explosion"
-                  }
+                target_effects = {
+                    {
+                        type = "damage",
+                        damage = {amount = 600 , type = "physical"}
+                    },
+                    {
+                        type = "damage",
+                        damage = {amount = 300 , type = "explosion"}
+                    },
+                    {
+                        type = "create-entity",
+                        entity_name = "plutonium-cannon-explosion"
+                    }
                 }
-              }
             }
-          },
-          {
-            type = "create-entity",
-            entity_name = "small-scorchmark",
-            check_buildability = true
-          }
+        },
+        final_action = {
+            type = "direct",
+            action_delivery = {
+                type = "instant",
+                target_effects = {
+                    {
+                        type = "create-entity",
+                        entity_name = "small-scorchmark",
+                        check_buildability = true
+                    }
+                }
+            }
+        },
+        animation = {
+            filename = "__base__/graphics/entity/bullet/bullet.png",
+            frame_count = 1,
+            width = 3,
+            height = 50,
+            priority = "high"
         }
-      }
     },
-    animation =
     {
-      filename = "__base__/graphics/entity/bullet/bullet.png",
-      frame_count = 1,
-      width = 3,
-      height = 50,
-      priority = "high"
+        type = "projectile",
+        name = "explosive-plutonium-cannon-projectile",
+        flags = {"not-on-map"},
+        collision_box = {{-0.3, -1.1}, {0.3, 1.1}},
+        acceleration = 0,
+        piercing_damage = 150,
+        action = {
+            type = "direct",
+            action_delivery = {
+                type = "instant",
+                target_effects = {
+                    {
+                        type = "damage",
+                        damage = {amount = 525 , type = "physical"}
+                    },
+                    {
+                        type = "create-entity",
+                        entity_name = "plutonium-cannon-explosion"
+                    }
+                }
+            }
+        },
+        final_action = {
+            type = "direct",
+            action_delivery = {
+                type = "instant",
+                target_effects = {
+                    {
+                        type = "create-entity",
+                        entity_name = "plutonium-cannon-shell-explosion"
+                    },
+                    {
+                        type = "nested-result",
+                        action = {
+                            type = "area",
+                            radius = 4.25,
+                            action_delivery = {
+                                type = "instant",
+                                target_effects = {
+                                    {
+                                        type = "damage",
+                                        damage = {amount = 475, type = "explosion"}
+                                    },
+                                    {
+                                        type = "create-entity",
+                                        entity_name = "plutonium-cannon-explosion"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        type = "create-entity",
+                        entity_name = "small-scorchmark",
+                        check_buildability = true
+                    }
+                }
+            }
+        },
+        animation = {
+            filename = "__base__/graphics/entity/bullet/bullet.png",
+            frame_count = 1,
+            width = 3,
+            height = 50,
+            priority = "high"
+        }
     }
-  },
 })
