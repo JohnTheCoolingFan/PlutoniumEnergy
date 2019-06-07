9b0d13114a700cfc7da41733b6cec0242360592a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-06-08 00:26:44 +0300

    Updated technologies stats and soft-tabs

--- prototypes/technology/technologies.lua
+++ prototypes/technology/technologies.lua
@@ -1,74 +1,94 @@
 require("prototypes.technology.ammo")
 
 data:extend({
-	{
-		type = "technology",
-		name = "plutonium-nuclear-power",
-		icon = "__PlutoniumEnergy__/graphics/technology/plutonium-nuclear-power.png",
-		icon_size = 128,
-		effects = {
-			{
-				type = "unlock-recipe",
-				recipe = "MOX-fuel"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "MOX-reactor"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "advanced-nuclear-fuel-reprocessing"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "plutonium-238-nuclesynthesis"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "industrial-reactor"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "uranium-238-excess-neutron-capture"
-			}
-		},
-		prerequisites = {"nuclear-power"},
-		unit = {
-			ingredients = {
-				{"automation-science-pack", 1},
-				{"logistic-science-pack", 1},
-				{"chemical-science-pack", 1}
-			},
-			time = 35,
-			count = 1000
-		},
-		order = "e-p-b-d"
-	},
-	{
-		type = "technology",
-		name = "MOX-fuel-reprocessing",
-		icon = "__PlutoniumEnergy__/graphics/technology/MOX-fuel-reprocessing.png",
-		icon_size = 128,
-		effects = {
-			{
-				type = "unlock-recipe",
-				recipe = "MOX-fuel-reprocessing"
-			},
-			{
-				type = "unlock-recipe",
-				recipe = "plutonium-239-alpha-decay"
-			}
-		},
-		prerequisites = {"plutonium-nuclear-power"},
-		unit = {
-			ingredients = {
-				{"automation-science-pack", 1},
-				{"logistic-science-pack", 1},
-				{"chemical-science-pack", 1}
-			},
-			time = 25,
-			count = 1500
-		},
-		order = "c-a"
-	}
+    {
+        type = "technology",
+        name = "plutonium-processing",
+        icon_size = 128,
+        icon = "__base__/graphics/technology/uranium-processing.png", -- TODO
+        effects = {
+            {
+                type = "unlock-recipe",
+                recipe = "industrial-reactor"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "advanced-nuclear-fuel-reprocessing"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "plutonium-238-nuclesynthesis"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "uranium-238-excess-neutron-capture"
+            }
+        },
+        prerequisites = {"uranium-processing", "nuclear-fuel-reprocessing"},
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1}
+            },
+            time = 30,
+            count = 400
+        },
+        order = "e-p-b-d"
+      },
+    {
+        type = "technology",
+        name = "plutonium-nuclear-power",
+        icon = "__PlutoniumEnergy__/graphics/technology/plutonium-nuclear-power.png",
+        icon_size = 128,
+        effects = {
+            {
+            	type = "unlock-recipe",
+            	recipe = "MOX-fuel"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "MOX-reactor"
+            }
+        },
+        prerequisites = {"plutonium-processing"},
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1}
+            },
+            time = 35,
+            count = 1000
+        },
+        order = "e-p-b-d"
+    },
+    {
+        type = "technology",
+        name = "MOX-fuel-reprocessing",
+        icon = "__PlutoniumEnergy__/graphics/technology/MOX-fuel-reprocessing.png",
+        icon_size = 128,
+        effects = {
+            {
+                type = "unlock-recipe",
+                recipe = "MOX-fuel-reprocessing"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "plutonium-239-alpha-decay"
+            }
+        },
+        prerequisites = {"plutonium-nuclear-power"},
+        unit = {
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1},
+                {"production-science-pack", 1}
+            },
+            time = 30,
+            count = 200
+        },
+        order = "e-p-b-d"
+    }
 })
