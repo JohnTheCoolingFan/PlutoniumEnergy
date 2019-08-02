c72c4a8aaffee8087f085a6e3752f4bfecc75920
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-08-02 13:22:59 +0300

    Removed U-238 excess neutron capture recipe from technology effects

--- prototypes/technology/technologies.lua
+++ prototypes/technology/technologies.lua
@@ -18,10 +18,6 @@ data:extend({
             {
                 type = "unlock-recipe",
                 recipe = "plutonium-238-nuclesynthesis"
-            },
-            {
-                type = "unlock-recipe",
-                recipe = "uranium-238-excess-neutron-capture"
             }
         },
         prerequisites = {"uranium-processing", "nuclear-fuel-reprocessing"},
