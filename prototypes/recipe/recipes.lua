1cb66dc3b065e7258111da3322424a854e3b7d3e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-08-02 13:21:27 +0300

    Removed Uranium-238 excess neutron capture recipe

--- prototypes/recipe/recipes.lua
+++ prototypes/recipe/recipes.lua
@@ -54,22 +54,6 @@ data:extend({
     },
     {
         type = "recipe",
-        name = "uranium-238-excess-neutron-capture",
-        localised_name = {"recipe-name.uranium-238-excess-neutron-capture"},
-        energy_required = 10,
-        enabled = false,
-        category = "excess-neutron-capture",
-        ingredients = {{"uranium-238", 10}},
-        icon = "__PlutoniumEnergy__/graphics/icons/uranium-238-excess-neutron-capture.png",
-        icon_size = 32,
-        subgroup = "raw-material",
-        order = "m[excess-neutron-capture]",
-        results = {
-            {name = "PE-plutonium-239", amount = 2}
-        }
-    },
-    {
-        type = "recipe",
         name = "MOX-fuel",
         energy_required = 10,
         enabled = false,
