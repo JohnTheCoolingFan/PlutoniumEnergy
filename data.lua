3761bdd04d52c3d63066d3fb7e9b7ac42debb2b7
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-08-09 00:48:19 +0300

    Updated TODO

--- data.lua
+++ data.lua
@@ -5,3 +5,6 @@ require("prototypes.recipe.recipes")
 require("prototypes.technology.technologies")
 require("prototypes.categories.fuel-category")
 require("prototypes.categories.recipe-category")
+
+-- TODO
+-- Add option to disable thath recipe from 1.1.3 and 1.1.4
