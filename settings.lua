df05175e01c8cb48720fec5e41a0a3bd408f0ec0
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-08-12 17:55:49 +0300

    Added setting to disable U238 excesss neutron capture recipe

--- settings.lua
+++ settings.lua
@@ -4,5 +4,11 @@ data:extend({
         type = 'bool-setting',
         default_value = false,
         setting_type = 'startup'
+    },
+    {
+        name = 'PE-disable-U238-excess-neutron-capture-recipe',
+        type = 'bool-setting',
+        default_value = false,
+        setting_type = 'startup'
     }
 })
