faf36839b6a35210a03f584d0ec716c7b4f66c91
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-09-18 16:48:20 +0300

    At least *some* code formatting

--- prototypes/entity/entities.lua
+++ prototypes/entity/entities.lua
@@ -3,16 +3,21 @@ local entities = {}
 
 -- TODO: Fix heated reactor graphics
 local MOX_reactor = util.table.deepcopy(data.raw["reactor"]["nuclear-reactor"])
+
 MOX_reactor.name = "MOX-reactor"
 MOX_reactor.icon = "__PlutoniumEnergy__/graphics/icons/MOX-reactor.png"
 MOX_reactor.minable.result = "MOX-reactor"
 MOX_reactor.minable.mining_time = 1
 MOX_reactor.max_health = 400
+
 MOX_reactor.consumption = "20MW"
 MOX_reactor.energy_source.fuel_category = "PE-MOX"
+
 MOX_reactor.collision_box = {{-1.2, -1.2}, {1.2, 1.2}}
 MOX_reactor.selection_box = {{-1.45, -1.45}, {1.45, 1.45}}
+
 MOX_reactor.next_upgrade = nil
+
 local lower_layer = MOX_reactor.lower_layer_picture
 lower_layer.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes.png"
 lower_layer.width = 96
@@ -20,17 +25,22 @@ lower_layer.height = 92
 lower_layer.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes.png"
 lower_layer.hr_version.width = 192
 lower_layer.hr_version.height = 192
+
 MOX_reactor.heat_lower_layer_picture.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png'
 MOX_reactor.heat_lower_layer_picture.hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png'
+
 MOX_reactor.picture.layers[1].scale = 0.6
 MOX_reactor.picture.layers[1].hr_version.scale = 0.3
 MOX_reactor.picture.layers[2].scale = 0.6
 MOX_reactor.picture.layers[2].hr_version.scale = 0.3
+
 MOX_reactor.working_light_picture.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-lights-color.png"
 MOX_reactor.working_light_picture.scale = 0.6
 MOX_reactor.working_light_picture.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-lights-color.png"
 MOX_reactor.working_light_picture.hr_version.scale = 0.3
+
 MOX_reactor.light.color = {r = 0.0, g = 0.82, b = 0.9}
+
 MOX_reactor.heat_buffer.connections = {
     {
         position = {-1, -1},
@@ -65,19 +75,23 @@ MOX_reactor.heat_buffer.connections = {
         direction = defines.direction.west
     }
 }
+
 MOX_reactor.heat_buffer.heat_picture.scale = 0.6
 MOX_reactor.heat_duffer.heat_picture.hr_version.scale = 0.3
 MOX_reactor.heat_buffer.heat_glow.scale = 0.6
+
 local patches_pic_con = MOX_reactor.connection_patches_connected.sheet
 patches_pic_con.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png"
 patches_pic_con.variation_count = 12
 patches_pic_con.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png"
 patches_pic_con.hr_version.variation_count = 12
+
 local patches_pic_discon = MOX_reactor.connection_patches_disconnected.sheet
 patches_pic_discon.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png"
 patches_pic_discon.variation_count = 12
 patches_pic_discon.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png"
 patches_pic_discon.hr_version.variation_count = 12
+
 -- TODO: heat_connection_patches_connected
 -- TODO: heat_connection_patches_disconnected
 table.insert(entities, MOX_reactor)
