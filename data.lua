f1f0990126cfa6f416d947c56c3a7955e494c4b6
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-03-15 01:09:00 +0300

    Updated TODO

--- data.lua
+++ data.lua
@@ -7,4 +7,5 @@ require("prototypes.categories.fuel-category")
 require("prototypes.categories.recipe-category")
 
 -- TODO: Support for Schall Uranium Processing (https://mods.factorio.com/mod/PlutoniumEnergy/discussion/5de1876b082e2b000b966564) (https://mods.factorio.com/mod/SchallUraniumProcessing)
+--       Done: locale using references. TODO: swap locale strings when mod is detected. Also do recipe names without output count
 -- TODO: Remake icons for x64 size
