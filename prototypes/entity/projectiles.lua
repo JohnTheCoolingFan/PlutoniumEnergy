23cb2dd353c0b5cc4d141bb99f891190759617e3
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-08-15 11:19:37 +0300

    Added back plutonium atomic rocket

--- prototypes/entity/projectiles.lua
+++ prototypes/entity/projectiles.lua
@@ -13,16 +13,40 @@ for k, v in pairs(plutonium_cannon_explosion.animations) do
 end
 table.insert(projectiles, plutonium_cannon_explosion)
 
---[[
+
+-- atomic rocket * 1.4
 local plutonium_atomic_rocket = util.table.deepcopy(data.raw["projectile"]["atomic-rocket"])
 plutonium_atomic_rocket.name = "plutonium-atomic-rocket"
-plutonium_atomic_rocket.action.action_delivery.target_effects[1].repeat_count = 140
-plutonium_atomic_rocket.action.action_delivery.target_effects[3].damage.amount = 560
-plutonium_atomic_rocket.action.action_delivery.target_effects[6].radius = 10 -- 7 * 1.4 = 9.8
-plutonium_atomic_rocket.action.action_delivery.target_effects[7].action.repeat_count = 2800
-plutonium_atomic_rocket.action.action_delivery.target_effects[7].action.radius = 50 -- 35 * 1.4 = 49
+plutonium_atomic_rocket.action.action_delivery[1].radius = 17                           -- math.ceil(12 * 1.4)
+plutonium_atomic_rocket.action.action_delivery[2].radius = 13                           -- 9 * 1.4 = 12.6
+plutonium_atomic_rocket.action.action_delivery[7].damage.amount = 560                   -- 400 * 1.4
+plutonium_atomic_rocket.action.action_delivery[10].radius = 20                          -- 14 * 1.4
+plutonium_atomic_rocket.action.action_delivery[11].spawn_min_radius = 16.5 -- Rough numbers
+plutonium_atomic_rocket.action.action_delivery[11].spawn_max_radius = 17.5
+plutonium_atomic_rocket.action.action_delivery[11].spawn_min = 45
+plutonium_atomic_rocket.action.action_delivery[11].spawn_max = 60
+plutonium_atomic_rocket.action.action_delivery[12].action.repeat_count = 1400           -- 1000 * 1.4
+plutonium_atomic_rocket.action.action_delivery[12].action.radius = 10                   -- 7 * 1.4
+plutonium_atomic_rocket.action.action_delivery[12].action.action_delivery.starting_speed = 0.5*0.8 * 1.4
+plutonium_atomic_rocket.action.action_delivery[13].action.repeat_count = 1400           -- 1000 * 1.4
+plutonium_atomic_rocket.action.action_delivery[13].action.radius = 50                   -- 35 * 1.4
+plutonium_atomic_rocket.action.action_delivery[13].action.action_delivery.starting_speed = 0.5*0.7 * 1.4
+plutonium_atomic_rocket.action.action_delivery[14].action.repeat_count = 1400           -- 1000 * 1.4
+plutonium_atomic_rocket.action.action_delivery[14].action.radius = 36                   -- 26 * 1.4
+plutonium_atomic_rocket.action.action_delivery[14].action.action_delivery.starting_speed = 0.5*0.7 * 1.4
+plutonium_atomic_rocket.action.action_delivery[15].action.repeat_count = 980            -- 700 * 1.4
+plutonium_atomic_rocket.action.action_delivery[15].action.radius = 6                    -- 4 * 1.4
+plutonium_atomic_rocket.action.action_delivery[15].action.action_delivery.starting_speed = 0.5*0.65 * 1.4
+plutonium_atomic_rocket.action.action_delivery[16].action.repeat_count = 1400           -- 1000 * 1.4
+plutonium_atomic_rocket.action.action_delivery[16].action.radius = 11                   -- 8 * 1.4
+plutonium_atomic_rocket.action.action_delivery[16].action.action_delivery.starting_speed = 0.5*0.65 * 1.4
+plutonium_atomic_rocket.action.action_delivery[17].action.repeat_count = 420            -- 300 * 1.4
+plutonium_atomic_rocket.action.action_delivery[17].action.radius = 36                   -- 26 * 1.4
+plutonium_atomic_rocket.action.action_delivery[17].action.action_delivery.starting_speed = 0.5*0.65 * 1.4
+plutonium_atomic_rocket.action.action_delivery[18].action.repeat_count = 14             -- 10 * 1.4
+plutonium_atomic_rocket.action.action_delivery[18].action.radius = 11                   -- 8 * 1.4
 table.insert(projectiles, plutonium_atomic_rocket)
-]]
+
 
 if mods["AtomicArtillery"] then
     local plutonium_atomic_artillery_projectile = util.table.deepcopy(data.raw["artillery-projectile"]["atomic-artillery-projectile"])
