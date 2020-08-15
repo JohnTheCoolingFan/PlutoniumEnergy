9ce8a337acc3bae73f81d8a4950063056519c0ad
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-08-15 13:48:05 +0300

    Updated TODO

--- data.lua
+++ data.lua
@@ -6,7 +6,6 @@ require("prototypes.technology.technologies")
 require("prototypes.categories.fuel-category")
 require("prototypes.categories.recipe-category")
 
--- TODO: Make better description
 -- TODO: Support for Schall Uranium Processing (https://mods.factorio.com/mod/PlutoniumEnergy/discussion/5de1876b082e2b000b966564) (https://mods.factorio.com/mod/SchallUraniumProcessing)
 --       I have and idea to make PE's recipes similar to SUP's recipes.
 -- TODO: Support for SChall Artillery (https://mods.factorio.com/mod/SchallArtillery)
