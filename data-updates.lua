a8b1e06eb4fd9b4577c5333f928cde4a83da65f5
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-08-21 17:59:10 +0300

    bob plates integration

--- data-updates.lua
+++ data-updates.lua
@@ -1,1 +1,15 @@
 data.raw['item']['MOX-fuel'].icon = '__PlutoniumEnergy__/graphics/icons/MOX-fuel.png'
+
+if mods['bobplates'] then
+    -- Disable some recipes
+    data.raw['technology']['plutonium-processing'].effects.remove(2)
+    data.raw['technology']['plutonium-processing'].effects.remove(4)
+    data.raw['technology']['MOX-fuel-reprocessing'].effects.remove(2)
+
+    -- Change recipes ingredients/results
+    data.raw['recipe']['MOX-fuel'].ingredients[3][1] = 'plutonium-239'
+    data.raw['recipe']['MOX-fuel-reprocessing'].results[1].name = 'plutinium-239'
+
+    -- Ammo
+    data.raw['recipe']['plutonium-atomic-bomb'].ingredients[3][1] = 'plutonium-239'
+end
