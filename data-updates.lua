494cb0c1ffe18c819ec76bb2305f5eda98189f9f
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-08-21 18:06:58 +0300

    Fix typos

--- data-updates.lua
+++ data-updates.lua
@@ -2,13 +2,13 @@ data.raw['item']['MOX-fuel'].icon = '__PlutoniumEnergy__/graphics/icons/MOX-fuel
 
 if mods['bobplates'] then
     -- Disable some recipes
-    data.raw['technology']['plutonium-processing'].effects.remove(2)
-    data.raw['technology']['plutonium-processing'].effects.remove(4)
-    data.raw['technology']['MOX-fuel-reprocessing'].effects.remove(2)
+    table.remove(data.raw['technology']['plutonium-processing'].effects, 2)
+    table.remove(data.raw['technology']['plutonium-processing'].effects, 4)
+    table.remove(data.raw['technology']['MOX-fuel-reprocessing'].effects, 2)
 
     -- Change recipes ingredients/results
     data.raw['recipe']['MOX-fuel'].ingredients[3][1] = 'plutonium-239'
-    data.raw['recipe']['MOX-fuel-reprocessing'].results[1].name = 'plutinium-239'
+    data.raw['recipe']['MOX-fuel-reprocessing'].results[1].name = 'plutonium-239'
 
     -- Ammo
     data.raw['recipe']['plutonium-atomic-bomb'].ingredients[3][1] = 'plutonium-239'
