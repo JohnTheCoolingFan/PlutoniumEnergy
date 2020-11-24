dcf1baf718794e52dc88ab80776de7c2d88bbf90
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-11-25 00:36:11 +0300

    Fixed reactor for 1.1 (at least it works, TODO)

--- prototypes/entity/entities.lua
+++ prototypes/entity/entities.lua
@@ -1,3 +1,5 @@
+-- TODO: redo reactor graphics with new glow.
+
 require "util"
 local entities = {}
 
@@ -26,12 +28,12 @@ lower_layer.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-react
 lower_layer.hr_version.width = 192
 lower_layer.hr_version.height = 192
 
-MOX_reactor.heat_lower_layer_picture.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png'
-MOX_reactor.heat_lower_layer_picture.width = 96
-MOX_reactor.heat_lower_layer_picture.height = 96
-MOX_reactor.heat_lower_layer_picture.hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png'
-MOX_reactor.heat_lower_layer_picture.hr_version.width = 192
-MOX_reactor.heat_lower_layer_picture.hr_version.height = 192
+MOX_reactor.heat_lower_layer_picture.layers[1].filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png'
+MOX_reactor.heat_lower_layer_picture.layers[1].width = 96
+MOX_reactor.heat_lower_layer_picture.layers[1].height = 96
+MOX_reactor.heat_lower_layer_picture.layers[1].hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png'
+MOX_reactor.heat_lower_layer_picture.layers[1].hr_version.width = 192
+MOX_reactor.heat_lower_layer_picture.layers[1].hr_version.height = 192
 
 MOX_reactor.picture.layers[1].scale = 0.6
 MOX_reactor.picture.layers[1].shift = util.by_pixel(-4, -6)
@@ -45,7 +47,7 @@ MOX_reactor.working_light_picture.scale = 0.6
 MOX_reactor.working_light_picture.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-lights-color.png"
 MOX_reactor.working_light_picture.hr_version.scale = 0.3
 
-MOX_reactor.light.color = {r = 0.0, g = 0.82, b = 0.9}
+--MOX_reactor.light.color = {r = 0.0, g = 0.82, b = 0.9}
 
 MOX_reactor.heat_buffer.connections = {
     {
@@ -82,9 +84,9 @@ MOX_reactor.heat_buffer.connections = {
     }
 }
 
-MOX_reactor.heat_buffer.heat_picture.scale = 0.6
-MOX_reactor.heat_buffer.heat_picture.hr_version.scale = 0.3
-MOX_reactor.heat_buffer.heat_glow.scale = 0.6
+MOX_reactor.heat_buffer.heat_picture.layers[1].scale = 0.6
+MOX_reactor.heat_buffer.heat_picture.layers[1].hr_version.scale = 0.3
+--MOX_reactor.heat_buffer.heat_glow.scale = 0.6
 
 local patches_pic_con = MOX_reactor.connection_patches_connected.sheet
 patches_pic_con.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png"
@@ -95,12 +97,12 @@ patches_pic_discon.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/M
 patches_pic_discon.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png"
 
 local patches_pic_con_heat = MOX_reactor.heat_connection_patches_connected.sheet
-patches_pic_con_heat.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
-patches_pic_con_heat.hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
+patches_pic_con_heat.layers[1].filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
+patches_pic_con_heat.layers[1].hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
 
 local patches_pic_discon_heat = MOX_reactor.heat_connection_patches_disconnected.sheet
-patches_pic_discon_heat.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
-patches_pic_discon_heat.hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
+patches_pic_discon_heat.layers[1].filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
+patches_pic_discon_heat.layers[1].hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
 table.insert(entities, MOX_reactor)
 
 
