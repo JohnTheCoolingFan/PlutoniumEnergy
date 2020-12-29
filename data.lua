32a5af13a6a3885c5e2bfaafa9ad48200ed331d9
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-12-29 03:09:07 +0300

    Updated TODO

--- data.lua
+++ data.lua
@@ -6,6 +6,7 @@ require("prototypes.technology.technology")
 require("prototypes.categories.fuel-category")
 require("prototypes.categories.recipe-category")
 
+-- TODO: add remnants (MOX reactor)
 -- TODO: Support for Schall Uranium Processing (https://mods.factorio.com/mod/PlutoniumEnergy/discussion/5de1876b082e2b000b966564) (https://mods.factorio.com/mod/SchallUraniumProcessing)
 --       I have and idea to make PE's recipes similar to SUP's recipes.
 -- TODO: Support for SChall Artillery (https://mods.factorio.com/mod/SchallArtillery)
