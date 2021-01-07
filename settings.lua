b5cfe654d59750984911070b313d165cb1a2436d
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-01-07 21:33:26 +0300

    Some annotations

--- settings.lua
+++ settings.lua
@@ -6,13 +6,13 @@ data:extend({
         setting_type = 'startup'
     },
     {
-        name = 'PE-disable-U238-excess-neutron-capture-recipe',
+        name = 'PE-disable-U238-excess-neutron-capture-recipe', -- Considered to be removed when corresponding recipe will be removed
         type = 'bool-setting',
         default_value = false,
         setting_type = 'startup'
     },
     {
-        name = 'PE-SUP-advanced-nfr-uranium-concentrate',
+        name = 'PE-SUP-advanced-nfr-uranium-concentrate',       -- Considered to be removed because not needed
         type = 'bool-setting',
         default_value = true,
         setting_type = 'startup'
