7a221d1ffd9c59e7770f50a9bf25a8b8e2e07a2e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-01-07 20:53:05 +0300

    TODO for next release

--- data.lua
+++ data.lua
@@ -7,6 +7,17 @@ require("prototypes.technology.technology")
 require("prototypes.categories.fuel-category")
 require("prototypes.categories.recipe-category")
 
+-- TODO for next release:
+--   - New plutonium enrichment process
+--     In:              Out:
+--       - 10x U-235      5x U-235
+--       - 20x U-238      10x U-238
+--       - 30x Pu-239     45x Pu-239
+--       - 10x Pu-238     2x Pu-238
+--   - Remove excess nutron capture recipe
+--   - Remove prefix
+
+
 -- TODO: add remnants (MOX reactor)
 -- TODO: Support for Schall Uranium Processing (https://mods.factorio.com/mod/PlutoniumEnergy/discussion/5de1876b082e2b000b966564) (https://mods.factorio.com/mod/SchallUraniumProcessing)
 --       I have and idea to make PE's recipes similar to SUP's recipes.
