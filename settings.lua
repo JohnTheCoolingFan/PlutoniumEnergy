4f3b3427e864ea4eb1f57a8804dda024777e037e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-01-07 21:38:40 +0300

    Removed som settings

--- settings.lua
+++ settings.lua
@@ -4,17 +4,17 @@ data:extend({
         type = 'bool-setting',
         default_value = false,
         setting_type = 'startup'
-    },
-    {
+    }
+    --[[{
         name = 'PE-disable-U238-excess-neutron-capture-recipe', -- Considered to be removed when corresponding recipe will be removed
         type = 'bool-setting',
         default_value = false,
         setting_type = 'startup'
-    },
-    {
+    },]]
+    --[[{
         name = 'PE-SUP-advanced-nfr-uranium-concentrate',       -- Considered to be removed because not needed
         type = 'bool-setting',
         default_value = true,
         setting_type = 'startup'
-    }
+    }]]
 })
