523cd1bcd667dbe8dcd13135deed277888985206
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-01-07 21:39:17 +0300

    Updated TODO

--- data.lua
+++ data.lua
@@ -7,14 +7,5 @@ require("prototypes.technology.technology")
 require("prototypes.categories.fuel-category")
 require("prototypes.categories.recipe-category")
 
--- TODO for next release:
---   - New plutonium enrichment process
---     In:              Out:
---       - 10x U-235      12x U-235
---       - 20x U-238      10x U-238
---       - 30x Pu-239     45x Pu-239
---       - 10x Pu-238     2x Pu-238
---   - Remove excess nutron capture recipe
-
 -- TODO: add remnants (MOX reactor)
 -- TODO: Support for SChall Artillery (https://mods.factorio.com/mod/SchallArtillery)
