d0b88ec2b720a96d1d4322c877607a7029ce9db2
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-01-08 17:11:14 +0300

    Updated TODO

--- data.lua
+++ data.lua
@@ -7,5 +7,4 @@ require("prototypes.technology.technology")
 require("prototypes.categories.fuel-category")
 
 -- TODO: better advanced reprocessing process (used-up fuel + acid = components)
--- TODO: add remnants (MOX reactor)
 -- TODO: Support for SChall Artillery (https://mods.factorio.com/mod/SchallArtillery)
