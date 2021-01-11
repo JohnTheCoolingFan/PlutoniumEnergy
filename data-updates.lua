aab316798606024a2f9f5a7e11460dc921f4f452
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-01-11 15:48:10 +0300

    Updated bobplates compatibility code

--- data-updates.lua
+++ data-updates.lua
@@ -2,7 +2,7 @@ data.raw['item']['MOX-fuel'].icon = '__PlutoniumEnergy__/graphics/icons/MOX-fuel
 
 if mods['bobplates'] then
     -- Disable some recipes
-    table.remove(data.raw['technology']['plutonium-processing'].effects, 2)
-    table.remove(data.raw['technology']['plutonium-processing'].effects, 4)
-    table.remove(data.raw['technology']['MOX-fuel-reprocessing'].effects, 2)
+    data.raw['technology']['plutonium-enrichment-process'].hidden = true
+    data.raw['technology']['plutonium-atomic-bomb'].prerequisites = {'atomic-bomb'}
+    data.raw['technology']['plutonium-processing'].effects = {}
 end
