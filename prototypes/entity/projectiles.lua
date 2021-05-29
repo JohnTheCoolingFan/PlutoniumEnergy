a37fafbc67366b21ce1737f6e1c8b9ebabcfeded
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-05-29 14:20:34 +0300

    Naming

--- prototypes/entity/projectiles.lua
+++ prototypes/entity/projectiles.lua
@@ -10,8 +10,8 @@ table.insert(projectiles, plutonium_cannon_shell_explosion)
 
 local plutonium_cannon_explosion = util.table.deepcopy(data.raw["explosion"]["explosion"])
 plutonium_cannon_explosion.name = "plutonium-cannon-explosion"
-for k, v in pairs(plutonium_cannon_explosion.animations) do
-    v.tint = {r = 0.1, g = 0.9, b = 0.7}
+for _, spritesheet in pairs(plutonium_cannon_explosion.animations) do
+    spritesheet.tint = {r = 0.1, g = 0.9, b = 0.7}
 end
 table.insert(projectiles, plutonium_cannon_explosion)
 
