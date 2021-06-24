d03c48aafceda8ec6fe83256ddde714ea5011a5f
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-06-24 20:13:56 +0300

    Space Exploration compatibility by Polish DeathCare

--- data.lua
+++ data.lua
@@ -9,3 +9,9 @@ require("prototypes.categories.fuel-category")
 -- TODO: New screenshots, better overview
 -- TODO: better advanced reprocessing process (used-up fuel + acid [in chemical plant] = components)
 -- TODO: SchallRadioactiveWaste. Check if integration is already there and add if not.
+
+-- Space Exploration compatibility by Polish DeathCare
+if not se_delivery_cannon_recipes then se_delivery_cannon_recipes = {} end
+
+se_delivery_cannon_recipes["plutonium-239"] = {name="plutonium-239"}
+se_delivery_cannon_recipes["plutonium-238"] = {name="plutonium-238"}
