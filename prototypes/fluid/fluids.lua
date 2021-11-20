a9f8ff7cd16074a738da9c364724144810efab53
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-11-20 17:12:16 +0300

    Used up nuclear fuel solution

---
+++ prototypes/fluid/fluids.lua
@@ -0,0 +1,11 @@
+data:extend({
+        {
+            type = "fluid",
+            name = "used-up-nuclear-fuel-solution",
+            base_color = {0.55, 0.60, 0.1},
+            flow_color = {0.5, 1.0, 0.9},
+            default_temperature = 0,
+            icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+            icon_size = 64, icon_mipmaps = 4
+        }
+})
