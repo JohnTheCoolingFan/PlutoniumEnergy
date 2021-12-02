69ac1c2ff236974427cc9ce4690357bef5f1f372
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-12-02 18:51:42 +0300

    Use the new icon

--- prototypes/fluid/fluids.lua
+++ prototypes/fluid/fluids.lua
@@ -5,7 +5,7 @@ data:extend({
             base_color = {r=0.55, g=0.60, b=0.40},
             flow_color = {r=0.60, g=0.90, b=0.40},
             default_temperature = 0,
-            icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+            icon = "__PlutoniumEnergy__/graphics/icons/used-up-uranium-fuel-cell-solution.png",
             icon_size = 64, icon_mipmaps = 4
         }
 })
