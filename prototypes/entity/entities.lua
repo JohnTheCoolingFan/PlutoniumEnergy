a283ab0c1281275b88e1c2e1976366b108fcddff
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-01-05 16:14:46 +0300

    Redefine MOX reactor

--- prototypes/entity/entities.lua
+++ prototypes/entity/entities.lua
@@ -2,123 +2,237 @@
 
 require "util"
 
-local entities = {}
+data:extend({
+        {
+            type = 'reactor',
+            name = 'MOX-reactor',
+            icon = '__PlutoniumEnergy__/graphics/icons/MOX-reactor.png',
+            icon_size = 64, icon_mipmaps = 4,
+            flags = {'placeable-neutral', 'player-creation'},
+            minable = {mining_time = 0.5, result = 'MOX-reactor'},
+            max_health = 400,
+            corpse = 'MOX-reactor-remnants',
+            dying_explosion = 'nuclear-reactor-explosion',
+            consumption = '20MW',
+            neighbour_bonus = 1,
+            energy_source = {
+                type = 'burner',
+                fuel_category = 'MOX',
+                effectivity = 1,
+                fuel_inventory_size = 1,
+                burnt_inventory_size = 1,
+                light_flicker = {
+                    color = {0, 0, 0},
+                    minimum_intensity = 0.7,
+                    maximum_intensity = 0.95
+                }
+            },
+            collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+            selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+            damaged_trigger_effect = data.raw['reactor']['nuclear-reactor'].damaged_trigger_effect,
+            --TODO: reexamine heat pipes sprites and maybe remake them, adjust shifts
+            lower_layer_picture = {
+                filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes.png',
+                width = 96,
+                height = 92,
+                shift = util.by_pixel(-2, -4),
+                hr_version = {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes.png',
+                    width = 192,
+                    height = 192,
+                    scale = 0.5
+                    shift = util.by_pixel(-1, -5)
+                }
+            },
+            heat_lower_layer_picture = apply_heat_pipe_glow {
+                filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png',
+                width = 96,
+                height = 96,
+                shift = util.by_pixel(-3, -4),
+                hr_version = {
+                    filename = '__PlutoniumEnergy__/graphics/Entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png',
+                    width = 192,
+                    height = 192,
+                    scale = 0.5,
+                    shift = util.by_pixel(-0.5, -4.5)
+                }
+            },
 
-local MOX_reactor = util.table.deepcopy(data.raw["reactor"]["nuclear-reactor"])
+            picture = {
+                layers = {
+                    {
+                        filename = '__base__/graphics/entity/nuclear-reactor/reactor.png',
+                        width = 154,
+                        height = 158,
+                        shift = util.by_pixel(-4, -6),
+                        scale = 0.6,
+                        hr_version = {
+                            filename = '__base__/graphics/entity/nuclear-reactor/hr-reactor.png',
+                            width = 302,
+                            height = 318,
+                            scale = 0.3,
+                            shift = util.by_pixel(-3, -7)
+                        }
+                    },
+                    {
+                        filename = '__base__/graphics/entity/nuclear-reactor/reactor-shadow.png',
+                        width = 263,
+                        height = 162,
+                        scale = 0.6,
+                        shift = {1.625, 0},
+                        draw_as_shadow = true,
+                        hr_version = {
+                            filename = '__base__/graphics/entity/nuclear-reactor/hr-reactor-shadow.png',
+                            width = 525,
+                            height = 323,
+                            scale = 0.3,
+                            shift = {1.625, 0},
+                            draw_as_shadow = true
+                        }
+                    }
+                }
+            },
 
-MOX_reactor.name = "MOX-reactor"
-MOX_reactor.icon = "__PlutoniumEnergy__/graphics/icons/MOX-reactor.png"
-MOX_reactor.minable.result = "MOX-reactor"
-MOX_reactor.max_health = 400
-MOX_reactor.corpse = 'MOX-reactor-remnants'
+            working_light_picture = {
+                filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-lights-color.png',
+                blend_mode = 'additive',
+                draw_as_glow = true,
+                width = 160,
+                height = 160,
+                scale = 0.6,
+                shift = {-0.03125, -0.1875},
+                hr_version = {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-lights-color.png',
+                    blend_mode = 'additive',
+                    draw_as_glow = true,
+                    width = 320,
+                    height = 320,
+                    scale = 0.3,
+                    shift = {-0.03125, -0.1875}
+                }
+            },
 
-MOX_reactor.consumption = "20MW"
-MOX_reactor.energy_source.fuel_category = "MOX"
+            heat_buffer = {
+                max_temperature = 1000,
+                specific_heat = '10MJ',
+                max_transfer = '10GW',
+                minimum_glow_temperature = 350,
+                connections = {
+                    {
+                        position = {-1, -1},
+                        direction = defines.direction.north
+                    },
+                    {
+                        position = {1, -1},
+                        direction = defines.direction.north
+                    },
+                    {
+                        position = {1, -1},
+                        direction = defines.direction.east
+                    },
+                    {
+                        position = {1, 1},
+                        direction = defines.direction.east
+                    },
+                    {
+                        position = {1, 1},
+                        direction = defines.direction.south
+                    },
+                    {
+                        position = {-1, 1},
+                        direction = defines.direction.south
+                    },
+                    {
+                        position = {-1, 1},
+                        direction = defines.direction.west
+                    },
+                    {
+                        position = {-1, -1},
+                        direction = defines.direction.west
+                    }
+                },
+                heat_picture = apply_heat_pipe_glow {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png',
+                    width = 96,
+                    height = 96,
+                    shift = util.by_pixel(1, -7),
+                    hr_version = {
+                        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png',
+                        width = 192,
+                        height = 192,
+                        scale = 0.5,
+                        shift = util.by_pixel(3, -6.5)
+                    }
+                }
+            },
+            connection_patches_connected = {
+                sheet = {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png',
+                    width = 32,
+                    height = 32,
+                    variation_count = 8,
+                    hr_version = {
+                        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png',
+                        width = 64,
+                        height = 64,
+                        variation_count = 8,
+                    }
+                }
+            },
+            connection_patches_disconnected = {
+                sheet = {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png',
+                    width = 32,
+                    height = 32,
+                    variation_count = 8,
+                    y = 32,
+                    hr_version = {
+                        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png',
+                        width = 64,
+                        height = 64,
+                        variation_count = 8,
+                        y = 64,
+                        scale = 0.5
+                    }
+                }
+            },
+            heat_connection_patches_connected = {
+                sheet = {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
+                    width = 32,
+                    height = 32,
+                    variation_count = 8
+                    hr_version = {
+                        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
+                        width = 64,
+                        height = 64,
+                        variation_count = 8,
+                        scale = 0.5
+                    }
+                }
+            },
+            heat_connection_patches_disconnected = {
+                sheet = {
+                    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
+                    width = 32,
+                    height = 32,
+                    variation_count = 8,
+                    y = 32,
+                    hr_version = {
+                        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
+                        width = 64,
+                        height = 64,
+                        variation_count = 8,
+                        y = 64,
+                        scale = 0.5
+                    }
+                }
+            },
 
-MOX_reactor.collision_box = {{-1.2, -1.2}, {1.2, 1.2}}
-MOX_reactor.selection_box = {{-1.5, -1.5}, {1.5, 1.5}}
-
---TODO: reexamine heat pipes sprites and maybe remake them, adjust shifts
-local lower_layer = MOX_reactor.lower_layer_picture
-lower_layer.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes.png"
-lower_layer.width = 96
-lower_layer.height = 92
-lower_layer.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes.png"
-lower_layer.hr_version.width = 192
-lower_layer.hr_version.height = 192
-
-MOX_reactor.heat_lower_layer_picture = apply_heat_pipe_glow
-{
-    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png',
-    width = 96,
-    height = 96,
-    shift = util.by_pixel(-3, -4),
-    hr_version = {
-        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png',
-        width = 192,
-        height = 192,
-        scale = 0.5,
-        shift = util.by_pixel(-0.5, -4.5)
-    }
-}
-MOX_reactor.heat_buffer.heat_picture = apply_heat_pipe_glow
-{
-    filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-pipes-heated.png',
-    width = 96,
-    height = 96,
-    shift = util.by_pixel(1, -7),
-    hr_version = {
-        filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-pipes-heated.png',
-        width = 192,
-        height = 192,
-        scale = 0.5,
-        shift = util.by_pixel(3, -6.5)
-    }
-}
-
-MOX_reactor.picture.layers[1].scale = 0.6
-MOX_reactor.picture.layers[1].shift = util.by_pixel(-4, -6)
-MOX_reactor.picture.layers[1].hr_version.scale = 0.3
-MOX_reactor.picture.layers[1].hr_version.shift = util.by_pixel(-3, -7)
-MOX_reactor.picture.layers[2].scale = 0.6
-MOX_reactor.picture.layers[2].hr_version.scale = 0.3
-
-MOX_reactor.working_light_picture.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-lights-color.png"
-MOX_reactor.working_light_picture.scale = 0.6
-MOX_reactor.working_light_picture.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-lights-color.png"
-MOX_reactor.working_light_picture.hr_version.scale = 0.3
-
-MOX_reactor.heat_buffer.connections = {
-    {
-        position = {-1, -1},
-        direction = defines.direction.north
-    },
-    {
-        position = {1, -1},
-        direction = defines.direction.north
-    },
-    {
-        position = {1, -1},
-        direction = defines.direction.east
-    },
-    {
-        position = {1, 1},
-        direction = defines.direction.east
-    },
-    {
-        position = {1, 1},
-        direction = defines.direction.south
-    },
-    {
-        position = {-1, 1},
-        direction = defines.direction.south
-    },
-    {
-        position = {-1, 1},
-        direction = defines.direction.west
-    },
-    {
-        position = {-1, -1},
-        direction = defines.direction.west
-    }
-}
-
-local patches_pic_con = MOX_reactor.connection_patches_connected.sheet
-patches_pic_con.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png"
-patches_pic_con.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png"
-
-local patches_pic_discon = MOX_reactor.connection_patches_disconnected.sheet
-patches_pic_discon.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches.png"
-patches_pic_discon.hr_version.filename = "__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches.png"
-
-local patches_pic_con_heat = MOX_reactor.heat_connection_patches_connected.sheet
-patches_pic_con_heat.layers[1].filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
-patches_pic_con_heat.layers[1].hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
-
-local patches_pic_discon_heat = MOX_reactor.heat_connection_patches_disconnected.sheet
-patches_pic_discon_heat.layers[1].filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/MOX-reactor-connect-patches-heated.png'
-patches_pic_discon_heat.layers[1].hr_version.filename = '__PlutoniumEnergy__/graphics/entity/MOX-reactor/hr-MOX-reactor-connect-patches-heated.png'
-table.insert(entities, MOX_reactor)
-
-
-data:extend(entities)
+            vehicle_impact_sound = data.raw['reactor']['nuclear-reactor'].vehicle_impact_sound
+            open_sound = data.raw['reactor']['nuclear-reactor'].open_sound
+            close_sound = data.raw['reactor']['nuclear-reactor'].close_sound
+            working_sound = data.raw['reactor']['nuclear-reactor'].working_sound
+            meltdown_action = data.raw['reactor']['nuclear-reactor'].meltdown_action
+        }
+    })
