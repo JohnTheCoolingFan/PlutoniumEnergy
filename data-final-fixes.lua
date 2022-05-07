b7f8956788811b09abe1950cd1a091e965295742
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-05-07 13:08:18 +0300

    Added solution centrifuging to productivity whitelist

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,5 @@
 -- Add some recipes to productivty modules limitation (which is whitelist for some reason)
-local limitation_to_add = {'advanced-nuclear-fuel-reprocessing', 'MOX-fuel', 'MOX-fuel-reprocessing', 'plutonium-fuel', 'plutonium-enrichment-process'}
+local limitation_to_add = {'advanced-nuclear-fuel-reprocessing', 'MOX-fuel', 'MOX-fuel-reprocessing', 'plutonium-fuel', 'plutonium-enrichment-process', 'used-up-uranium-fuel-cell-solution-centrifuging'}
 for _, module in pairs(data.raw['module']) do
     if module.limitation and module.effect.productivity then
         for _, recipe in pairs(limitation_to_add) do
