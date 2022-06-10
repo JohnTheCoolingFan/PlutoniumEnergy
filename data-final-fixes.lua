07e62c676ec6947a56fb89be600fc1cda93cf693
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-06-10 19:56:31 +0300

    Added other recipes to productivity whitelist

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,15 @@
 -- Add some recipes to productivty modules limitation (which is whitelist for some reason)
-local limitation_to_add = {'advanced-nuclear-fuel-reprocessing', 'MOX-fuel', 'MOX-fuel-reprocessing', 'plutonium-fuel', 'plutonium-enrichment-process', 'used-up-uranium-fuel-cell-solution-centrifuging', 'breeder-fuel-cell-reprocessing'}
+local limitation_to_add = {
+    'advanced-nuclear-fuel-reprocessing',
+    'MOX-fuel', 'MOX-fuel-reprocessing',
+    'plutonium-fuel',
+    'plutonium-enrichment-process',
+    'used-up-uranium-fuel-cell-solution-centrifuging',
+    'breeder-fuel-cell-reprocessing',
+    'breeder-fuel-cell',
+    'breeder-fuel-cell-from-uranium-cell',
+    'breeder-fuel-cell-from-MOX-fuel'
+}
 for _, module in pairs(data.raw['module']) do
     if module.limitation and module.effect.productivity then
         for _, recipe in pairs(limitation_to_add) do
