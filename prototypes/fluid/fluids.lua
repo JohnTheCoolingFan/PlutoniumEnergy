bedd95f8cd63b01c445940780e8df8b4ca0ec0ce
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-06-22 15:11:16 +0300

    Added breeder fuel cell solution

--- prototypes/fluid/fluids.lua
+++ prototypes/fluid/fluids.lua
@@ -1,11 +1,22 @@
 data:extend({
-        {
-            type = "fluid",
-            name = "used-up-uranium-fuel-cell-solution",
-            base_color = {r=0.55, g=0.60, b=0.40},
-            flow_color = {r=0.60, g=0.90, b=0.40},
-            default_temperature = 0,
-            icon = "__PlutoniumEnergy__/graphics/icons/used-up-uranium-fuel-cell-solution.png",
-            icon_size = 64, icon_mipmaps = 4
-        }
+    {
+        type = "fluid",
+        name = "used-up-uranium-fuel-cell-solution",
+        base_color = { r = 0.55, g = 0.60, b = 0.40 },
+        flow_color = { r = 0.60, g = 0.90, b = 0.40 },
+        default_temperature = 0,
+        icon = "__PlutoniumEnergy__/graphics/icons/used-up-uranium-fuel-cell-solution.png",
+        icon_size = 64,
+        icon_mipmaps = 4
+    },
+    {
+        type = "fluid",
+        name = "used-up-breeder-fuel-cell-solution",
+        base_color = { r = 0.50, g = 0.40, b = 0.60 },
+        flow_color = { r = 0.60, g = 0.40, b = 0.90 },
+        default_temperature = 0,
+        icon = "__PlutoniumEnergy__/graphics/icons/used-up-uranium-fuel-cell-solution.png", -- TODO
+        icon_size = 64,
+        icon_mipmaps = 4
+    }
 })
