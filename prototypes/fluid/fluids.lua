6e154134011295b3e77be00ca46f79978deba53e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-08-10 06:24:06 +0300

    Coded in the new fluid icon

--- prototypes/fluid/fluids.lua
+++ prototypes/fluid/fluids.lua
@@ -15,7 +15,7 @@ data:extend({
         base_color = { r = 0.50, g = 0.40, b = 0.60 },
         flow_color = { r = 0.60, g = 0.40, b = 0.90 },
         default_temperature = 0,
-        icon = "__PlutoniumEnergy__/graphics/icons/used-up-uranium-fuel-cell-solution.png", -- TODO
+        icon = "__PlutoniumEnergy__/graphics/icons/used-up-breeder-fuel-cell-solution.png",
         icon_size = 64,
         icon_mipmaps = 4
     }
