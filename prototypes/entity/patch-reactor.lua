1a1d3a230037e2396b225dce98eb1dff67d98d9a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-09-30 20:59:40 +0300

    Add check for RealisticReactorGlow

--- prototypes/entity/patch-reactor.lua
+++ prototypes/entity/patch-reactor.lua
@@ -1,13 +1,16 @@
 -- This file patches the vanilla reactor to have a glow that is tinted based on fuel
 
--- Patch reactor prototype
+-- Check if Realistic Reactor Glow is present, don't tweak if it is installed.
+if not mods["REalisticReactorGlow"] then
+    -- Patch reactor prototype
 
-local nuclear_reactor = data.raw['reactor']['nuclear-reactor']
+    local nuclear_reactor = data.raw['reactor']['nuclear-reactor']
 
-nuclear_reactor.working_light_picture.filename =
-'__PlutoniumEnergy__/graphics/entity/nuclear-reactor/reactor-lights.png'
-nuclear_reactor.working_light_picture.hr_version.filename =
-'__PlutoniumEnergy__/graphics/entity/nuclear-reactor/hr-reactor-lights.png'
+    nuclear_reactor.working_light_picture.filename =
+    '__PlutoniumEnergy__/graphics/entity/nuclear-reactor/reactor-lights.png'
+    nuclear_reactor.working_light_picture.hr_version.filename =
+    '__PlutoniumEnergy__/graphics/entity/nuclear-reactor/hr-reactor-lights.png'
 
-nuclear_reactor.use_fuel_glow_color = true               -- should use glow color from fuel item prototype as light color and tint for working_light_picture
-nuclear_reactor.default_fuel_glow_color = { 0, 1, 0, 0 } -- just green -- color used as working_light_picture tint for fuels that don't have glow color defined
+    nuclear_reactor.use_fuel_glow_color = true               -- should use glow color from fuel item prototype as light color and tint for working_light_picture
+    nuclear_reactor.default_fuel_glow_color = { 0, 1, 0, 0 } -- just green -- color used as working_light_picture tint for fuels that don't have glow color defined
+end
