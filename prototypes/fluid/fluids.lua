ea7e9fb5e0e1b2a98fd2a510e118bf354fbfeab6
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-10-02 15:42:20 +0300

    Updated waste solution icons

--- prototypes/fluid/fluids.lua
+++ prototypes/fluid/fluids.lua
@@ -2,8 +2,8 @@ data:extend({
     {
         type = "fluid",
         name = "used-up-uranium-fuel-cell-solution",
-        base_color = { r = 0.55, g = 0.60, b = 0.40 },
-        flow_color = { r = 0.60, g = 0.90, b = 0.40 },
+        base_color = { r = 0.0, g = 1.0, b = 0.1 },
+        flow_color = { r = 0.1, g = 0.8, b = 0.2 },
         default_temperature = 0,
         icon = "__PlutoniumEnergy__/graphics/icons/used-up-uranium-fuel-cell-solution.png",
         icon_size = 64,
@@ -12,8 +12,8 @@ data:extend({
     {
         type = "fluid",
         name = "used-up-breeder-fuel-cell-solution",
-        base_color = { r = 0.50, g = 0.40, b = 0.60 },
-        flow_color = { r = 0.60, g = 0.40, b = 0.90 },
+        base_color = { r = 0.0, g = 0.9, b = 0.9 },
+        flow_color = { r = 0.1, g = 0.8, b = 0.8 },
         default_temperature = 0,
         icon = "__PlutoniumEnergy__/graphics/icons/used-up-breeder-fuel-cell-solution.png",
         icon_size = 64,
