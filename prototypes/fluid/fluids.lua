32e80c6e791a3303c2bf858c86594e65fbadbdb9
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-10-26 16:21:16 +0300

    Fix subgroup for fluids

--- prototypes/fluid/fluids.lua
+++ prototypes/fluid/fluids.lua
@@ -2,6 +2,7 @@ data:extend({
     {
         type = "fluid",
         name = "used-up-uranium-fuel-cell-solution",
+        subgroup = "fluid",
         base_color = { r = 0.0, g = 1.0, b = 0.1 },
         flow_color = { r = 0.1, g = 0.8, b = 0.2 },
         default_temperature = 0,
@@ -12,6 +13,7 @@ data:extend({
     {
         type = "fluid",
         name = "used-up-breeder-fuel-cell-solution",
+        subgroup = "fluid",
         base_color = { r = 0.0, g = 0.9, b = 0.9 },
         flow_color = { r = 0.1, g = 0.8, b = 0.8 },
         default_temperature = 0,
