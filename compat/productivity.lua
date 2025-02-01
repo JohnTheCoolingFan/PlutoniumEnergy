8bd5a2a2e617c5a2950d317969240c89b57bc9f6
JohnTheCoolingFan <ivan8215145640@gmail.com>
2025-02-01 21:38:31 +0300

    A big ass note

--- compat/productivity.lua
+++ compat/productivity.lua
@@ -1,5 +1,19 @@
 log("Tweaking productivity restrictions")
 
+-- This solves an old problem of each productivity module having a list of recipes it can be used on.
+-- Recently (somewhere in 2.0) this has been changed to a setting in a recipe (big upside)
+-- This file does both: adding recipes ot a module if it has recipe limitations, and
+-- Adding `allow_productivity = true` to recipes as well, even though it is already set there.
+--
+-- Moving to a per-recipe setting only would be a good idea but that woudl mean any mod
+-- adding a productivity module using limitations is potentially broken slightly.
+-- In other words: API updates are hard.
+--
+-- Also proprs to DocJade's Baketorio vid for pointing this out, been making a fix for Baketorio
+-- before patching this mod and adding the fix here.
+--
+--                                                                              JohnTheCoolingFan
+
 -- Add some recipes to productivty modules limitation (which is whitelist for some reason)
 local limitation_to_add = {
     'advanced-nuclear-fuel-reprocessing',
